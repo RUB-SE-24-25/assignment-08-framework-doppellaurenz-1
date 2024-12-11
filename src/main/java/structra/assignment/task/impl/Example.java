2055a2c5321b4484f7a094fe991802280f876142
Anwender <laurenz.fromme@gmail.com>
2024-12-11 15:35:02 +0100

    bug fix public

--- src/main/java/structra/assignment/task/impl/Example.java
+++ src/main/java/structra/assignment/task/impl/Example.java
@@ -10,7 +10,7 @@ public class Example {
      * Create the GUI and show it. For thread safety, this method should be invoked from the
      * event-dispatching thread.
      */
-    private static void createAndShowGUI() {
+    public static void createAndShowGUI() {
         // Create and set up the window
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
