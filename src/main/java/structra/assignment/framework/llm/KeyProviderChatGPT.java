471091677f51693728bfb1064462fecd212bcf94
Laurenzfromme <67288721+Laurenzfromme@users.noreply.github.com>
2024-12-11 14:48:57 +0100

    Update KeyProviderChatGPT.java
    Bug Fix. Import Class

--- src/main/java/structra/assignment/framework/llm/KeyProviderChatGPT.java
+++ src/main/java/structra/assignment/framework/llm/KeyProviderChatGPT.java
@@ -1,3 +1,7 @@
+package structra.assignment.framework.llm;
+
+import structra.assignment.framework.llm.KeyProvider;
+
 public class KeyProviderChatGPT implements KeyProvider{
   private String key = "structra-1343abnc-dghpcybpcybub3qgb3vyigfwasbrzxksig5py2ugdhj5ihrobya6kq==";
 
