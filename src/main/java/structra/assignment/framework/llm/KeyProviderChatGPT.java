5d44658f1f4c20b5293d3e09bcfd3ab86effebba
Jonas Maduch <Jonas>
2024-12-11 14:51:30 +0100

    test

--- src/main/java/structra/assignment/framework/llm/KeyProviderChatGPT.java
+++ src/main/java/structra/assignment/framework/llm/KeyProviderChatGPT.java
@@ -3,9 +3,9 @@ package structra.assignment.framework.llm;
 import structra.assignment.framework.llm.KeyProvider;
 
 public class KeyProviderChatGPT implements KeyProvider{
-  private String key = "structra-1343abnc-dghpcybpcybub3qgb3vyigfwasbrzxksig5py2ugdhj5ihrobya6kq==";
+  private String KEY = "structra-1343abnc-dghpcybpcybub3qgb3vyigfwasbrzxksig5py2ugdhj5ihrobya6kq==";
 
   public String getApiKey(){
-    return this.key;
+    return this.KEY;
   }
 }
