1db3cebd7eec1cd9c61788a7614558f3fd748468
Jonas Maduch <Jonas>
2024-12-11 14:37:47 +0100

    eigene klasse

---
+++ src/main/java/structra/assignment/framework/llm/KeyProviderChatGPT.java
@@ -0,0 +1,7 @@
+public class KeyProviderChatGPT implements KeyProvider{
+  private String key = "structra-1343abnc-dghpcybpcybub3qgb3vyigfwasbrzxksig5py2ugdhj5ihrobya6kq==";
+
+  public String getApiKey(){
+    return this.key;
+  }
+}
